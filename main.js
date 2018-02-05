b290f1c37d870c81d2c6f49b099d0a8f8e9e537a
Arthur Smith <mirandasimpson@Mirandas-Air.attlocal.net>
2018-02-04 20:44:01 -0600

    it was broke so i pushed this up

--- main.js
+++ main.js
@@ -10,21 +10,21 @@ const treeObject = {
 //     const bottomWidth = treeObject.height.value;
 // };
 
-document.getElementById("submit-btn").addEventListener("click", function tree() {
+document.getElementById("submit-btn").addEventListener("click", function() {
     // console.log(treeObject.height.value);
     // console.log(treeObject.character.value);
         if (treeObject.height.value === "" || treeObject.character.value === "") {
             alert("Oops! You forgot one of the fields:(");
-        } 
-        //var grow = "";
+        };
+        var grow = "";
         var space = " ";
         const bottomWidth  = (treeObject.height.value * 2) - 1;
         //console.log(bottomWidth)
-        for (var i = 1; i <= bottomWidth; i += 2) {
-            space2 = space.repeat(treeObject.height.value - i / 2)
+        for (var i = 1; i < bottomWidth; i += 2) {
+            let space2 = space.repeat(treeObject.height.value - i / 2)
             grow = (treeObject.character.value).repeat(i);
             console.log(space2, grow);
-        }
+        };
     });
 
 
